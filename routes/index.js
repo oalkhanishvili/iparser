9868f0b517da34cacb85183db8d76a532194cca7
Otura <oalkhanishvili@gmail.com>
2016-12-12 19:02:01 +0400

    r

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,17 @@ var router = express.Router();
 var connection = require('../modules/database.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/api', function(req, res, next) {
   connection.query('SELECT * FROM colors', function(err, result) {
     if (err) throw err;
       res.json(result);
   });
 
+});
+router.post('/api/get-model', function(req, res, next) {
+  var request = req.body
+  ,params = request.params;
+  
+  res.json(req.body);
 });
 module.exports = router;
